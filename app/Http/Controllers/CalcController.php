93dd4f98c1f462493c92f56da1d9d4dff905316e
bb-hiroto-ishii <hiroto.ishii@bondband.jp>
2023-05-23 15:06:47 +0900

    main

---
+++ app/Http/Controllers/CalcController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Psr7\Request as Psr7Request;
+use Illuminate\Http\Request;
+
+class CalcController extends Controller
+{
+    public function calculator(Request $req)
+    {
+        $dsp = $req->name;
+        $j_in = $req->var0;
+        $dsp_old = $req->var1;
+        $j_dp = $req->var2;
+        $j_op = $req->var3;
+        return view('calculator',compact('dsp','j_in','dsp_old','j_dp','j_op'));
+    }
+    
+    public function result(Request $req)
+    {
+        $dsp = $req->name;
+        return view('result',compact('dsp'));
+    }
+}
