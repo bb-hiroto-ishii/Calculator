479258136a41e219266b3f1494b74e8139d71fe1
bb-hiroto-ishii <hiroto.ishii@bondband.jp>
2023-05-23 18:08:25 +0900

    calculate_test

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -3,21 +3,116 @@
 
 <head><title>test</title></head>
 <a href = "http://localhost/Calculator/public/calculator">戻る</a>
-<p>
 
+<p>
 <?PHP
 
     echo "電卓作成がんばろう！";
+?>
+</p>
+<p>
+    <?php
+    
 
-    $fm1 = "4";
-    $fm2 = "-2";
+    $dsp_old = "10+8-6*4/2=";
+
+    //i:何番目の文字を見るか
+    //j:何番目の配列に入れるか
+ 
+    ?>
+</p>
+<p>
+    <?php
+
+
+
+$j=0;
+for($i=0;$i<5;$i++){
+    $fm[$i]="";
+    $op[$i]="";
+}
+    for($i=0;$i<=strlen($dsp_old);$i++){
+        $t=substr($dsp_old,$i,1);
+        if($t=="="){
+            break;
+        }
+        if(
+            $t=="+"
+            || $t=="-"
+            || $t=="*"
+            || $t=="/"
+        ){
+            $op[$j]=$t;
+            $j++;
+        }else{
+            $fm[$j].=$t;
+        }
+    }
+
+    for($i=0;$i<5;$i++){
+        echo $fm[$i].$op[$i];
+    }
+
+    ?>
+</p><p>
+    <?php
+
+    for($i=0;$i<5;$i++){
+        if($op[$i]=="/"){
+            $fm[$i+1] = 1 / $fm[$i+1];
+            $op[$i] = "*";
+        }
+    }
+
+    for($i=0;$i<5;$i++){
+        echo $fm[$i].$op[$i];
+    }
+
+    ?>
+</p><p>
+    <?php
+
+    for($i=0;$i<5;$i++){
+        if($op[$i]=="-"){
+            $fm[$i+1] = -$fm[$i+1];
+            $op[$i] = "+";
+        }
+    }
+
+    for($i=0;$i<5;$i++){
+        echo $fm[$i].$op[$i];
+    }
+
+    ?>
+</p><p>
+    <?php
+
+//*1しかなくなるまで何回も回す
+    for($i=0;$i<5;$i++){
+        if($op[$i]=="*"){
+            $fm[$i] = $fm[$i] * $fm[$i+1];
+            $fm[$i+1] = "1";
+        }
+    }
+
+
+
+    for($i=0;$i<5;$i++){
+        echo $fm[$i].$op[$i];
+    }
+
+    $ans=0;
+    for($i=0;$i<5;$i++){
+        echo $ans += $fm[$i];
+    }
+
     
     //$fm1=var_dump((float)$fm1);
     //$fm2=var_dump((float)$fm2);
 
-    $ans = $fm1+$fm2;
+    //$ans = $fm1+$fm2;
 
-    echo $ans;
+    //echo $ans;
 
 
     /*
@@ -29,4 +124,5 @@
     */
 
 ?>
+</p>
 </html>
